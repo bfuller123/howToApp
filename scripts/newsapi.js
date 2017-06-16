4d90eba935315c01e32de316ad7369bb53b38ce8
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-06-15 21:15:12 -0500

    notes in newsapi file

---
+++ scripts/newsapi.js
@@ -0,0 +1,24 @@
+8bcf7b67fec74a178a6c2ce0e225b91a
+
+https://newsapi.org/v1/articles
+https://newsapi.org/v1/sources
+
+
+var queryURL = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=8bcf7b67fec74a178a6c2ce0e225b91a"
+
+
+https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=8bcf7b67fec74a178a6c2ce0e225b91a
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+}).done(function(response) {
+	console.log(response)
+});
+
+https://docs.webhose.io/
+
+http://eventregistry.org/searchEvents
+
+blog aggregator
+
