fdaa9268ae240a5d0b9670cdb0ea328d6703734c
JCFlores <jflores717@gmail.com>
2017-06-18 23:35:44 -0500

    code only pulls embeddable videos only

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,43 @@
 //Truncate string function for books and newsapi
 function truncateString(str, num) {
-    if (num > str.length) {
-        return str;
-    } else {
-        str = str.substring(0, num);
-        return str + "...";
-    }
+  if (num > str.length) {
+    return str;
+  } else {
+    str = str.substring(0, num);
+    return str + "...";
+  }
 }
 
 //Login Buttons
 $(".modal-close").on("click", function() {
-    $(".modal").removeClass("is-active");
-})
+  $(".modal").removeClass("is-active");
+});
 $(".modal-background").on("click", function() {
-    $(".modal").removeClass("is-active");
-})
+  $(".modal").removeClass("is-active");
+});
 
 $("#loginButton").on("click", function() {
-    $(".login").addClass("is-active");
-})
+  $(".login").addClass("is-active");
+});
 
 //On click to grab course which was clicked
 $(".card").on("click", function() {
-    localStorage.setItem("keyword", $(this).data("keyword"));
+  localStorage.setItem("keyword", $(this).data("keyword"));
 });
 
 //Carousel from slick
 $('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
 
 
 $('.calendar-carousel').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    accessibility: true,
-    arrows: true,
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  accessibility: true,
+  arrows: true,
 });
--- scripts/youtubeapi.js
+++ scripts/youtubeapi.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
   var youtubeAPI = {
     url: "https://www.googleapis.com/youtube/v3/search",
-    q: "?q=surfing",
+    part: "?part=snippet",
     results: "&maxResults=3",
-    part: "&part=snippet",
-    key: "&key=AIzaSyBnboNloog0d2dSKXk9zaz7FIR_p8JTLl4",
+    type: "&type=video",
+    q: "&q=surfing",
+    videoEmbed: "&videoEmbeddable=true",
+    key: "&key=AIzaSyBnboNloog0d2dSKXk9zaz7FIR_p8JTLl4"
   };
 
   $.ajax({
-    url: youtubeAPI.url + youtubeAPI.q + youtubeAPI.results + youtubeAPI.part + youtubeAPI.key,
-    method: "GET"
-  }).done(function(response) {
-    console.log(response);
-  });
+      url: youtubeAPI.url + youtubeAPI.part + youtubeAPI.results + youtubeAPI.type + youtubeAPI.q + youtubeAPI.videoEmbed + youtubeAPI.key,
+      method: "GET"
+    })
+    .done(function(response) {
+      for (var i = 0; i < response.items.length; i++) {
+        console.log(response.items[i]);
+      }
+    });
 
 });
 
-localStorage.getItem("keyword")
+localStorage.getItem("keyword");
